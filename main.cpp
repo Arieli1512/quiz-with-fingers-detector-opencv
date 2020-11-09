b59d113079a984973b368b7c0a25c490694398db
Michał <222609@edu.p.lodz.pl>
2020-11-09 13:27:01 +0100

    quiz + camera

--- main.cpp
+++ main.cpp
@@ -1,36 +1,88 @@
 ﻿#include <iostream>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
-#include "binaries/processing.h"
+#include "processing.h"
 
 using namespace cv;
 using namespace std;
 
+const char* questions[4] = { "Stolica Hiszpanii to ?",
+	"Kto byl pierwszym krolem Polski",
+	"Kto wygral MS w Pilce Noznej w 2018 roku?" };
+const char* answers[4][5] = {
+	{ "Barcelona", "Londyn", "Madryt","Grenada","Sewilla"},
+	{ "Mieszko I", "Boleslaw I Chrobry", "Kazimierz Wielki","Stansilaw Poniatowski","Mieszko II" },
+{"Polska","Niemcy","Rosja","Chorwacja","Francja"}
+};
+int correctAnswer[4] = { 3, 2, 5 };
+
+
 
 int main() {
 
-	String path = "palec5.jpg";
-	Mat thres = imageAfterPreprocessing(path);
-	if (thres.empty()) {
-		cout << "File not found" << endl;
-		return - 1;
-	}
-	vector<vector<Point>> contours  = findContoursImage(thres);
 
-	vector<vector<Point>> hull(contours.size());
-	vector<vector<int> > hulls2(contours.size());
-	vector<vector<Vec4i>> convDefect = createConvexity(hull, contours);
+		VideoCapture cam(0);
+
+		//String path = "finger5.jpg";
+		Mat img;
+		
+		int printFirst = 0, timerQ = 0, nextAnswer = 0;
+
+		while (1) {
 
 
-	Mat drawing = drawContoursImage(thres, contours, hull);
-	int counter = countFingers(contours, drawing, convDefect);
+			try {
+
+				cam.read(img);
+				
+				Mat thres = imageAfterPreprocessing(img);
+
+				if (thres.empty()) {
+					cout << "File not found" << endl;
+					return -1;
+				}
+				vector<vector<Point>> contours = findContoursImage(thres);
+				vector<vector<Point>> hull(contours.size());
+				vector<vector<int> > hulls2(contours.size());
+				vector<vector<Vec4i>> convDefect = createConvexity(hull, contours);
 
 
-	cout << "liczba palcow : " << counter << endl;
+				Mat drawing = drawContoursImage(thres, contours, hull);
+				int counter = countFingers(contours, drawing, convDefect);
 
-	imshow("window drawing", drawing);
+				if (printFirst == 0 && nextAnswer<3) {
+					cout << questions[nextAnswer]<<endl;
+					for (int i = 0; i < 5; i++) {
+						cout << i+1<<") "<< answers[nextAnswer][i] << endl;
+					}
+					printFirst = 1;
+				}
 
-	waitKey(0);
+				timerQ++;
+				if (timerQ == 150 && nextAnswer<3) {
+					if (counter == correctAnswer[nextAnswer]) {
+						cout << endl<<"Dobra odpowiedz!" << endl<<endl;
 
-	return 0;
+					}
+					else cout << endl<<"Zla odpowiedz!" << endl<<endl;
+					printFirst = 0;
+					timerQ = 0;
+					nextAnswer++;
+				}
+
+				
+				putText(drawing, to_string(counter), Point(50, 50), FONT_HERSHEY_SIMPLEX, 2, 255);
+				cv::imshow("window", drawing);
+				if (waitKey(30) == 27)return -1;
+
+			}
+			catch (cv::Exception& e)
+			{
+
+			}
+
+
+		}
+
+		return 0;	
 }
