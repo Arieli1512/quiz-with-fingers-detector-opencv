2b7760807332368f0d72572b3bbcf627d0e34a8f
Michał <222609@edu.p.lodz.pl>
2020-11-18 14:57:52 +0100

    h file

--- questions.h
+++ questions.h
@@ -6,36 +6,39 @@
 
 using namespace std;
 
-class Question{
-	private:
-		string question;
-		string optionA;
-		string optionB;
-		string optionC;
-		string optionD;
-		string optionE;
-		string answer;
+class Question {
+private:
+	string question;
+	string optionA;
+	string optionB;
+	string optionC;
+	string optionD;
+	string optionE;
+	string answer;
+	int answerNumber;
 
-	public:
-		void setQuestion(string question);
-		void setOptionA(string optionA);
-		void setOptionB(string optionB);
-		void setOptionC(string optionC);
-		void setOptionD(string optionD);
-		void setOptionE(string optionE);
-		void setAnswer(string answer);
+public:
+	void setQuestion(string question);
+	void setOptionA(string optionA);
+	void setOptionB(string optionB);
+	void setOptionC(string optionC);
+	void setOptionD(string optionD);
+	void setOptionE(string optionE);
+	void setAnswer(string answer);
+	void setAnswerNumber(int number);
 
-		string getQuestion();
-		string getOptionA();
-		string getOptionB();
-		string getOptionC();
-		string getOptionD();
-		string getOptionE();
-		string getAnswer();
+	string getQuestion();
+	string getOptionA();
+	string getOptionB();
+	string getOptionC();
+	string getOptionD();
+	string getOptionE();
+	string getAnswer();
+	int getAnswerNumber();
 
 };
 
-class QuestionList{
+class QuestionList {
 
 private:
 	vector<Question> questionArray;
@@ -43,5 +46,7 @@ public:
 	int readQuesionsFromFile(string pathToFile);
 	void printAllQuestions();
 	void printQuestion(int index);
+	string getAnswer(int index);
+	int getAnswerNumber(int index);
 	~QuestionList();
 };
