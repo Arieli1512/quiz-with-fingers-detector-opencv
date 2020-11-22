83d0a0fccbbeab99e17a2cffa482aba8314bd603
Michał <222609@edu.p.lodz.pl>
2020-11-22 23:53:49 +0100

    class Quiz

--- main.cpp
+++ main.cpp
@@ -5,132 +5,241 @@
 #include "questions.h"
 #include <SFML/Graphics.hpp>
 
-using namespace cv;
+//using namespace cv;
 using namespace std;
 using namespace sf;
 
-int main() {
-	
-		QuestionList questionList;
-		int totalQuestions = 10;
-		int x = questionList.readQuesionsFromFile("../pytania_projekt.txt");
+class Quiz {
+private:
+	RectangleShape questionBox;
+	vector<RectangleShape> answersBox;
+	QuestionList questionList;
+	FingersDetector fingersDetector;
 
-		if (x == -1) {
-			cout << "Nie mozna otworzyc pliku" << endl;
-			return -2;
-		}
-		RenderWindow window(VideoMode(1400, 650), "Quiz", Style::Close | Style::Resize);
-		RectangleShape pytanieBox(Vector2f(1400.0f, 400.0f));
+	Texture button, questionTexture;
+	Text textAnswers, textQuestions;
 
-		vector<RectangleShape> odpowiedziBox(5, RectangleShape(Vector2f(280, 300)));
-		string abc[5] = { "Zielony","Fioletowy", "Niebieski", "Czerwony", "Czarny", };
+	vector<Text> answersText;
+	Font font;
+	Event event;
+	Text score;
+	VideoCapture camera;
 
-		Texture button,pytanieTexture;
-		Font font;
+	int qs=0;
+	int timer=0;
+	int totalQuestions = 23;
+	int flag = 0;
 
-		Text textAnsweres("A",font);
-		textAnsweres.setCharacterSize(30);
-		Text textQuestions("A", font);
-		textQuestions.setCharacterSize(36);
+public:
+	RenderWindow window;
+	Quiz(RectangleShape questionBox, vector<RectangleShape> answersBox, QuestionList questionList, FingersDetector fingersDetector)
+		:window(VideoMode(1800, 650), "Quiz", Style::Close | Style::Resize) 
+	{
+		this->questionBox = questionBox;
+		this->answersBox = answersBox;
+		this->questionList = questionList;
+	}
 
-		vector<Text> odpowiedziText(5, textAnsweres);
-		
-		font.loadFromFile("../fonts/AllerDisplay.ttf");
-		button.loadFromFile("../imagesSfml/baner4.jpg");
+	void configCamera() {
+		VideoCapture cam1(0);
+		this->camera = cam1;
+	}
+	void setUp() {
 
-	
-		textQuestions.setFillColor(sf::Color::White);
-
-	
-		pytanieTexture.loadFromFile("../imagesSfml/button.png");
-
-		pytanieBox.setTexture(&button);
-		pytanieBox.setFillColor(sf::Color::White);
-		window.setFramerateLimit(60);
-		window.clear(sf::Color(255, 255, 255));
-		Event event;
-
-		int timer = 0, qs = 0, qsFlag = 0;
-
-		Text score("Pytanie 1/20", font);
-		textQuestions.setCharacterSize(36);
-
-		while (window.isOpen())
-		{
-
-			//window.clear(Color::White);
-			while (window.pollEvent(event))
-			{
-				switch (event.type)
-				{
-				case Event::Closed:
-					window.close();
-					break;
-				case Event::Resized:
-					//printf("New window width %i \nNew window height %i \n", event.size.width, event.size.height);
-					break;
-
-				}
+			int x = questionList.readQuesionsFromFile("../pytania_projekt.txt");
+			if (x == -1) {
+				return;
 			}
 
-			window.clear();
-			window.draw(pytanieBox);
+			textAnswers.setCharacterSize(36);
+			textQuestions.setCharacterSize(45);
 
+			textAnswers.setFont(font);
+			textQuestions.setFont(font);
+
+			vector<Text> answersTextx(5, textAnswers);
+			this->answersText = answersTextx;
+
+			string str1 = "Pytanie 1 / ";
+			str1.append(to_string(totalQuestions));
+			score.setString(str1);
 			
-			odpowiedziBox[1].setFillColor(sf::Color::Red);
-			odpowiedziBox[2].setFillColor(sf::Color::Blue);
-			odpowiedziBox[3].setFillColor(sf::Color(0, 153, 51));
-			odpowiedziBox[4].setFillColor(sf::Color(102, 20, 179));
-			odpowiedziBox[0].setFillColor(sf::Color::Magenta);
+			font.loadFromFile("../fonts/AllerDisplay.ttf");
+			button.loadFromFile("../imagesSfml/baner4.jpg");
+			questionTexture.loadFromFile("../imagesSfml/button.png");
+			score.setFont(font);
 
-	
-			pytanieBox.setPosition(0, 0);
-			textQuestions.setPosition(300, 170);
+			textQuestions.setFillColor(sf::Color::White);
+			answersBox[0].setFillColor(sf::Color::Magenta);
+			answersBox[1].setFillColor(sf::Color::Red);
+			answersBox[2].setFillColor(sf::Color::Blue);
+			answersBox[3].setFillColor(sf::Color(0, 153, 51));
+			answersBox[4].setFillColor(sf::Color(102, 20, 179));
+
+			questionBox.setTexture(&button);
+			questionBox.setFillColor(sf::Color::White);
+
+			questionBox.setOrigin(questionBox.getSize().x / 2.0f, questionBox.getSize().y / 2.0f);
+			questionBox.setPosition(900, 200);
+			
+			textQuestions.setPosition(900, 200);
+			window.setFramerateLimit(60);
+			//window.clear(sf::Color(255, 255, 255));
+			Text score("Pytanie 1/20", font);
+
 		
-			if (timer == 200) {
+			for (int i = 0; i < answersBox.size(); i++) {
+				answersText[i].setFillColor(Color::White);
+				answersBox[i].setOrigin(answersBox[i].getSize().x/2.0f, answersBox[i].getSize().y / 2.0f);
+				answersText[i].setOrigin(answersText[i].getLocalBounds().width/2.0f, answersText[i].getLocalBounds().height / 2.0f);
+				answersBox[i].setPosition(180.0f + 360.0f * i, 550.0f);
+				answersText[i].setPosition(answersBox[i].getPosition());
+				answersText[i].setPosition(180.0f + 360.0f * i, 500.0f);
+			}
+		
+
+	}
+
+	void handleEvent() {
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case Event::Closed:
+				window.close();
+				break;
+			case Event::Resized:
+				break;
+
+			}
+		}
+	}
+
+
+	int handleAnswers() {
+
+		//try {
+
+			cv::Mat img;
+			camera.read(img);
+			Mat thres = fingersDetector.processingImage(img);
+			if (thres.empty()) {
+				cout << "File not found" << endl;
+				return -1;
+			}
+			vector<vector<cv::Point>> contours = fingersDetector.findContoursImage(thres);
+			vector<vector<cv::Point>> hull(contours.size());
+			vector<vector<int> > hulls2(contours.size());
+			vector<vector<cv::Vec4i>> convDefect = fingersDetector.createConvexity(hull, contours);
+			cv::Mat drawing = fingersDetector.drawContoursImage(thres, contours, hull);
+			int numOfFingers = fingersDetector.countFingers(contours, drawing, convDefect);
+			
+			//window.clear();
+			window.draw(questionBox);
+			Text textP;
+			switch (flag) {
+			case 1:
+				textP.setString("Poprawna odpowiedz");
+				break;
+			case 2:
+				textP.setString("Bledna odpowiedz");
+				break;
+			}
+			textP.setPosition(1500, 10);
+			textP.setFont(font);
+			textP.setCharacterSize(30);
+
+			if (timer ==180) {
+				
+
+				if (numOfFingers == questionList.getQuestionArray().at(qs).getCorrectAnswer()) {
+					//textP.setString("Poprawna odpowiedz\n");
+					flag = 1;
+					cout << "POprawna odp" << numOfFingers <<"  corr answ :"<< questionList.getQuestionArray().at(qs).getCorrectAnswer()<<endl;
+				}
+				else {
+					//textP.setString("Bledna odpowiedz\n");
+					flag = 2;
+					cout << "Zla odp" << numOfFingers << "  corr answ :" << questionList.getQuestionArray().at(qs).getCorrectAnswer() << endl;
+				}
+				
+				
 				timer = 0;
 				qs++;
-				qsFlag = qs-1;
 				string mystr = "Pytanie ";
-				mystr.append(to_string(qs+1));
-				mystr.append("/20");
+				mystr.append(to_string(qs + 1));
+				mystr.append(" / ");
+				mystr.append(to_string(totalQuestions));
 				score.setString(mystr);
+
+			}
+
+			window.draw(textP);
+			for (int i = 0; i < answersBox.size(); i++){
+
+	
+				if (qs == totalQuestions)
+					return 1;
+				wstring questionString = questionList.getQuestionArray().at(qs).getQuestion();
+				wstring answersString = questionList.getQuestionArray().at(qs).getAnswers().at(i);
+
+				textQuestions.setString(sf::String::fromUtf8(questionString.begin(), questionString.end()));
+				answersText[i].setString(sf::String::fromUtf8(answersString.begin(), answersString.end()));
+				
+				textQuestions.setOrigin(textQuestions.getLocalBounds().width / 2.0f, textQuestions.getLocalBounds().height / 2.0f);
+				answersText[i].setOrigin(answersText[i].getLocalBounds().width / 2.0f, answersText[i].getLocalBounds().height / 2.0f);
+				window.draw(answersBox[i]);
+				window.draw(answersText[i]);
+				window.draw(textQuestions);
 				
 			}
-			
-				for (int i = 0; i < odpowiedziBox.size(); ++i)
-				{
-			
-			
-				
-					wstring questionString =questionList.getQuestionArray().at(qs).getQuestion();
-					textQuestions.setString(sf::String::fromUtf8(questionString.begin(), questionString.end()));
-					wstring answersString = questionList.getQuestionArray().at(qs).getAnswers().at(i);
-					odpowiedziText[i].setString(sf::String::fromUtf8(answersString.begin(), answersString.end()));
-	
 
-					odpowiedziText[i].setFillColor(sf::Color::White);
-					odpowiedziBox[i].setPosition(0.0f + 280.0f * i, 400.0f);
-					odpowiedziText[i].setPosition(80.0f + 280.0f * i, 500.0f);
-					window.draw(odpowiedziBox[i]);
-					window.draw(odpowiedziText[i]);
-					window.draw(textQuestions);
-				}
+
 			score.setPosition(10, 10);
 			window.draw(score);
-				
-
 			window.display();
 
 			timer++;
+			cv::putText(drawing, to_string(numOfFingers), cv::Point(50, 50), FONT_HERSHEY_SIMPLEX, 2, 255);
+			cv::imshow("window", drawing);
+			
+			if (waitKey(30) == 27)
+				return -1;
+
+		return 0;
+
+	}
+
+};
+
+
+
+
+
+
+int main() {
+	
+
+		RectangleShape questionBox(Vector2f(1800.0f, 400.0f));
+		vector<RectangleShape> answersBox(5, RectangleShape(Vector2f(360, 300)));
+		QuestionList questionList;
+		FingersDetector fingersDetector;
+
+		Quiz mainController(questionBox, answersBox, questionList, fingersDetector);
+
+		mainController.setUp();
+		mainController.configCamera();
+
+		while (true){
+
+			try {
+				mainController.handleEvent();
+				mainController.handleAnswers();
+			}
+			catch (cv::Exception& e) {
+				cout << e.what();
+			}
 		}
-
-
-
-
-
-
-
 
 		//Mat img;
 		//VideoCapture cam(0);
