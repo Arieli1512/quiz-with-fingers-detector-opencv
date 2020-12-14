34ab1ef473129cd8d79af7ad252375d66aa5dff1
Michał <222609@edu.p.lodz.pl>
2020-12-14 17:06:12 +0100

    add readQuestion + readAnswers

--- QuestionList.cpp
+++ QuestionList.cpp
@@ -5,57 +5,60 @@ vector < Question > QuestionList::getQuestionArray() {
 }
 
 
-int QuestionList::readQuesionsFromFile(string pathToFile) {
+wstring QuestionList::readQuestion(wifstream& file) {
+	wstring textQuestion;
+	getline(file, textQuestion);
+	size_t len = textQuestion.length();
+
+
+	if (textQuestion.at(len - 1) != '\?') {
+		wstring nextLine;
+		getline(file, nextLine);
+		textQuestion += '\n';
+		textQuestion.append(nextLine);
+	}
+	return textQuestion;
+}
+
+vector<wstring> QuestionList::readAnswers(wifstream& file) {
+	int numOfLines = 5;
+	vector < wstring > texts;
+	for (int i = 0; i < numOfLines; i++) {
+
+		wstring text;
+		getline(file, text);
+		texts.push_back(text);
+	}
+	return texts;
+}
+
+int QuestionList::readFromFile(string pathToFile) {
 
 	wifstream file;
 	file.open(pathToFile.c_str());
-	if (file.fail() == true) {
-
+	if (file.fail() == true)
 		return -1;
-	}
-
-	int numOfLines = 5;
+	
 	while (!file.eof()) {
-
 		Question question;
-		wstring textQuestion;
-
-		getline(file, textQuestion);
-		size_t len = textQuestion.length();
-		//wcout << textQuestion<<endl;
-		//wcout << textQuestion.at(len-1) << endl;
-		
-		if (textQuestion.at(len-1)!='\?') {
-			wstring nextLine;
-			getline(file, nextLine);
-			textQuestion += '\n';
-			textQuestion.append(nextLine);
-		}
+		wstring textQuestion = readQuestion(file);
 		question.setQuestion(textQuestion);
-
-		vector < wstring > texts;
-		for (int i = 0; i < numOfLines; i++) {
-
-			wstring text;
-			getline(file, text);
-			texts.push_back(text);
-		}
+		vector < wstring > texts = readAnswers(file);
 		question.setAnswers(texts);
-		//question.print();
-
 		question.mixAnswers();
 		wstring correctAnswerName;
 		getline(file, correctAnswerName);
-
 		question.setCorrectAnswerName(correctAnswerName);
 		question.compareAnswers(correctAnswerName);
-
 		questionArray.push_back(question);
 	}
-	file.close();
 
+	file.close();
 	return 0;
 }
+
+
+
 
 void QuestionList::mixQuestions() {
 	srand(unsigned(time(NULL)));
